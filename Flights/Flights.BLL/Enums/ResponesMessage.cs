25a60a0563e4f79aea436e7795bf4ccb11096d1d
fshariati61 <53239159+fshariati61@users.noreply.github.com>
2019-08-01 12:17:17 -0400

    Add files via upload

---
+++ Flights/Flights.BLL/Enums/ResponesMessage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.ComponentModel;
+
+namespace Flights.BLL.Enums
+{
+    /// <summary>
+    /// Enumaration class containing the response messages
+    /// </summary>
+    public class ResponesMessage
+    {
+        [DataContract]
+        public enum Message
+        {
+            [EnumMember]
+            [Description("The path is returned successfully")]
+            Successful,
+
+            [EnumMember]
+            [Description("An exception occured")]
+            SystemException,
+
+            [EnumMember]
+            [Description("The route was not found")]
+            RouteNotFound,
+
+
+            [EnumMember]
+            [Description("The origin was not found")]
+            InvalidOrigin,
+
+            [EnumMember]
+            [Description("The destination was not found")]
+            InvalidDestination,
+
+
+        }
+    }
+}
