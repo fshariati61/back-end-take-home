0447eb56fd00fc2c02b291b05e1c38fb885393e0
fshariati61 <53239159+fshariati61@users.noreply.github.com>
2019-08-01 12:26:04 -0400

    Add files via upload

---
+++ Flights/Flights.API/FlightsREST.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Flights.API
+{
+    public class FlightsREST: ServiceContracts.IFlightsREST
+    {
+        /// <summary>
+        /// Returns the shortest path between orgin and the destination
+        /// </summary>
+        /// <param name="Origin"></param>
+        /// <param name="Destination"></param>
+        public Flights.BLL.Contracts.FlightsRestAPIResult GetShortestRoute(string Origin, string Destination)
+        {
+            return (BLL.RoutingBLL.GetShortestRoute(Origin, Destination));
+        }
+
+    }
+}
