6e16b4d0ff4f5be8538d760dee73c5fd98935425
fshariati61 <53239159+fshariati61@users.noreply.github.com>
2019-08-01 12:19:28 -0400

    Add files via upload

---
+++ Flights/Flights.BLL/FindRoute.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights.BLL
+{
+    public class Graph
+    {
+        // adjacency list 
+        private Dictionary<string, List<string>> keyValueListDict;
+        private List<string> valueList; 
+
+        public Graph()
+        {
+            // initialise adjacency list  
+            InitAdjList();
+        }
+
+        private void InitAdjList()
+        {
+            keyValueListDict = new Dictionary<string, List<string>>();
+            valueList = new List<string>();
+        }
+
+        // Adds edge from origin to the destination  
+        public void AddEdge(string origin, string destination)
+        {
+            if (keyValueListDict.ContainsKey(origin))
+            {
+                List<string> list_temp = new List<string>();
+                list_temp = keyValueListDict[origin];
+
+
+                if (list_temp.Contains(destination))
+                {
+                   // Console.WriteLine("key = {0} , list contains = {1} ", origin, destination);
+                }
+                else
+                {
+                    list_temp.Add(destination);
+                }
+            }
+            else
+            {
+                keyValueListDict.Add(origin, new List<string> { destination });
+            }
+
+        }
+
+
+        // Prints all paths from  'origin' to 'destination'  
+         public string PrintAllPaths(string origin, string destination)
+        {
+            string resultPath = string.Empty;
+
+            Dictionary<string, bool> Visited = new Dictionary<string, bool>();
+            foreach (var kvp in keyValueListDict)
+            {
+                Visited[kvp.Key] = false;
+            }
+
+            List<string> pathList = new List<string>();
+
+            // add source to path[]  
+            pathList.Add(origin);
+
+            // Call recursive utility  
+            resultPath = PrintAllPathsUtil(origin, destination, Visited, pathList);
+            return (resultPath);
+        }
+
+        // A recursive function to print all paths from source to the destination.  
+        private string PrintAllPathsUtil(string source, string destination, Dictionary<string, bool> isVisited, List<string> localPathList)
+        {
+
+            // Mark the current node 
+            string result = string.Empty;
+            isVisited[source] = true;
+
+            if (source.Equals(destination))
+            {
+                result = string.Join("->", localPathList);
+
+                // if match found then no need to traverse more till depth
+                isVisited[source] = false;
+
+                return result;
+            }
+
+            // Recur for all the vertices adjacent to current vertex  
+            foreach (string indx in keyValueListDict[source])
+            {
+                if (!isVisited[indx])
+                {
+                    // store current node in path[]  
+                    localPathList.Add(indx);
+
+                    result = PrintAllPathsUtil(indx, destination, isVisited, localPathList);
+
+                    localPathList.Remove(indx);
+                }
+            }
+
+            // Mark the current node  
+            isVisited[source] = false;
+            return result;
+        }
+    }
+}
+
---
+++ Flights/Flights.BLL/RoutingBLL.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flights.DAL;
+
+namespace Flights.BLL
+{
+    public class RoutingBLL
+    {
+        public static List<Contracts.DataObjects.Airline> GetAirlines()
+        {
+            List<Contracts.DataObjects.Airline> result = new List<Contracts.DataObjects.Airline>();
+            using (var db = new DAL.FlightsEntities())
+            {
+                var airlines = db.Airlines.ToList();
+
+                foreach (var airline in airlines)
+                {
+                    result.Add(Contracts.CopyContract.CopyAirline(airline));
+                }
+
+                return result;
+            }
+        }
+
+        public static Contracts.DataObjects.Airline GetAirline(string twoDigitCode)
+        {
+            Contracts.DataObjects.Airline result = new Contracts.DataObjects.Airline();
+            using (var db = new DAL.FlightsEntities())
+            {
+                var airline = db.Airlines.Where(t => t.TwoDigitCode == twoDigitCode).FirstOrDefault();
+
+                return Contracts.CopyContract.CopyAirline(airline);
+            }
+        }
+
+        public static List<DAL.Route> GetAllRoutes()
+        {
+            List<DAL.Route> result = new List<DAL.Route>();
+            using (var db = new DAL.FlightsEntities())
+            {
+                return (db.Routes.ToList());
+            }
+        }
+
+
+        public static List<Contracts.DataObjects.Route> GetRoutes(string origin, string destination)
+        {
+            List<Contracts.DataObjects.Route> result = new List<Contracts.DataObjects.Route>();
+            using (var db = new DAL.FlightsEntities())
+            {
+                var routes = db.Routes.Where(r => r.Origin == origin && r.Destination == destination).ToList();
+
+                foreach (var route in routes)
+                {
+                    result.Add(Contracts.CopyContract.CopyRoute(route));
+                }
+
+                return result;
+            }
+
+        }
+
+        public static List<Contracts.DataObjects.Route> GetRoutes(string origin)
+        {
+            List<Contracts.DataObjects.Route> result = new List<Contracts.DataObjects.Route>();
+            using (var db = new DAL.FlightsEntities())
+            {
+                var routes = db.Routes.Where(r => r.Origin == origin).ToList();
+
+                foreach (var route in routes)
+                {
+                    result.Add(Contracts.CopyContract.CopyRoute(route));
+                }
+                return result;
+            }
+
+        }
+
+        public static int GetRoutesCount()
+        {
+            using (var db = new DAL.FlightsEntities())
+            {
+                return (db.Routes.ToList().Count);
+            }
+        }
+
+  
+        /// <summary>
+        /// Returns the shortetst route between the origin and destination
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="destination"></param>
+        /// <returns>FlightsRestAPIResult which contains the message and the path</returns>
+        public static BLL.Contracts.FlightsRestAPIResult GetShortestRoute(string origin, string destination)
+        {
+            Contracts.FlightsRestAPIResult result = new Contracts.FlightsRestAPIResult();
+            try
+            {
+                 using (var db = new DAL.FlightsEntities())
+                {
+                    var routes = db.Routes.Where(r => r.Origin == origin).ToList();
+                     if (routes.Count() < 1)
+                    {
+                        result.Message = Enums.ResponesMessage.Message.InvalidOrigin.ToString();
+                        return (result);
+                    }
+
+                    var desRoutes = db.Routes.Where(r => r.Destination == destination).ToList();
+                    if (desRoutes.Count() < 1)
+                    {
+                        result.Message = Enums.ResponesMessage.Message.InvalidDestination.ToString();
+                        return (result);
+                    }
+                }
+
+
+                Graph g = new Graph();
+                List<DAL.Route> lstRoute = GetAllRoutes();
+                foreach (var route in lstRoute)
+                {
+                    g.AddEdge(route.Origin, route.Destination);
+                }
+
+                result.Message = Enums.ResponesMessage.Message.Successful.ToString();
+                result.Path = g.PrintAllPaths(origin, destination);
+                return (result);
+            }
+            catch
+            {
+                result.Message = Enums.ResponesMessage.Message.SystemException.ToString();
+                return (result);
+            }
+        }
+
+    }
+
+}
+
+
