475ac3d5f480735a2bd198e2f5198500543ce2ba
fshariati61 <53239159+fshariati61@users.noreply.github.com>
2019-08-01 12:25:31 -0400

    Add files via upload

---
+++ Flights/Flights.API/ServiceContracts/IFlightsRestAPI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using Flights.BLL.Contracts;
+
+namespace Flights.API.ServiceContracts
+{
+    [ServiceContract(Namespace = "http://FlightsRESTAPI")]
+    public interface IFlightsREST
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, UriTemplate = "GetRoute/?Origin={Origin}&Destination={Destination}", ResponseFormat = WebMessageFormat.Json)]
+        FlightsRestAPIResult GetShortestRoute(string Origin, string Destination);
+
+    }
+}
